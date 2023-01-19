7d89ac485fa43125a64f30df133f5f5032d6bdc2
Alexandr <kobal2009@yandex.ru>
2023-01-19 23:38:31 +0500

    Fix Timer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,10 +1,9 @@
 export const timer = (deadline) => {
-    const timerDays = document.getElementById('timer-days')
-    const timerHours = document.getElementById('timer-hours')
-    const timerMinutes = document.getElementById('timer-minutes')
-    const timerSeconds = document.getElementById('timer-seconds')
+    const timerDays = document.querySelectorAll('.timer-days')
+    const timerHours = document.querySelectorAll('.timer-hours')
+    const timerMinutes = document.querySelectorAll('.timer-minutes')
+    const timerSeconds = document.querySelectorAll('.timer-seconds')
     let timeInterval
-
     const getTimeRemaining = () => {
         let dateStop = new Date(deadline).getTime()
         let dateNow = new Date().getTime()
@@ -16,28 +15,58 @@ export const timer = (deadline) => {
         
         return {timeRemaining, days, hours, minutes, seconds}
     }
-    function showTimer(days, hours, minutes, seconds) {
-        let getTime = getTimeRemaining()
-        if (getTime.timeRemaining <= 0) {
-            clearInterval(timeInterval)
-            timerDays.textContent = '00'
-            timerHours.textContent = '00'
-            timerMinutes.textContent = '00'
-            timerSeconds.textContent = '00'
-            return
+
+    timerDays.forEach(item => {
+        function showTimer(days, hours, minutes, seconds) {
+            let getTime = getTimeRemaining()
+            if (getTime.timeRemaining <= 0) {
+                clearInterval(timeInterval)
+                item.textContent = '00'
+                return
+            }
+            item.textContent = getTime.days
         }
-        timerDays.textContent = getTime.days
-        timerHours.textContent = getTime.hours >= 10 ? getTime.hours : `0${getTime.hours}`
-        timerMinutes.textContent = getTime.minutes >= 10 ? getTime.minutes : `0${getTime.minutes}`
-        timerSeconds.textContent = getTime.seconds >= 10 ? getTime.seconds : `0${getTime.seconds}`
-        
-    }
-    showTimer()
-    timeInterval = setInterval(showTimer, 1000)
-
-
-
-
-
+        showTimer()
+        timeInterval = setInterval(showTimer, 1000)
+    })
+    timerHours.forEach(item => {
+        function showTimer(days, hours, minutes, seconds) {
+            let getTime = getTimeRemaining()
+            if (getTime.timeRemaining <= 0) {
+                clearInterval(timeInterval)
+                item.textContent = '00'
+                return
+            }
+            item.textContent = getTime.hours >= 10 ? getTime.hours : `0${getTime.hours}`
+        }
+        showTimer()
+        timeInterval = setInterval(showTimer, 1000)
+    })
+    timerMinutes.forEach(item => {
+        function showTimer(days, hours, minutes, seconds) {
+            let getTime = getTimeRemaining()
+            if (getTime.timeRemaining <= 0) {
+                clearInterval(timeInterval)
+                item.textContent = '00'
+                return
+            }
+            item.textContent = getTime.minutes >= 10 ? getTime.minutes : `0${getTime.minutes}`
+        }
+        showTimer()
+        timeInterval = setInterval(showTimer, 1000)
+    })
+    timerSeconds.forEach(item => {
+        function showTimer(days, hours, minutes, seconds) {
+            let getTime = getTimeRemaining()
+            if (getTime.timeRemaining <= 0) {
+                clearInterval(timeInterval)
+                item.textContent = '00'
+                return
+            }
+            item.textContent = getTime.seconds >= 10 ? getTime.seconds : `0${getTime.seconds}`
+        }
+        showTimer()
+        timeInterval = setInterval(showTimer, 1000)
+    })
 
 }
