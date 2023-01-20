2dd77d68b1f66799f2e58efb851aa5a03155b2f4
Alexandr <kobal2009@yandex.ru>
2023-01-20 23:36:20 +0500

    Bugfix scroll

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,12 +1,12 @@
 export const scroll = () => {
     const btnTop = document.querySelector('.smooth-scroll')
-
+    btnTop.style.display = 'none'
 window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop >= 860) {
+    if (document.documentElement.scrollTop >= 857) {
         btnTop.style.display = 'block'
     } else {btnTop.style.display = 'none'}
     btnTop.addEventListener('click', () => {
-        window.scrollTo({
+        document.documentElement.scrollTo({
             top: 0,
             behavior: "smooth"
           })
